c642b82ff765fd864efa265425b9c390e65fb12c
CalebScott0 <calebbenscott@gmail.com>
2024-11-14 23:23:34 -0600

    setting up auth functionality

--- Review-Site-App/src/pages/HomePage.jsx
+++ Review-Site-App/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 const HomePage = () => {
   return (
-    <div className=" text-center tracking-wide text-xl pt-44">We home baby</div>
+    <div className=" text-center tracking-wide text-xl pt-44">Home Page</div>
   );
 };
 export default HomePage;
